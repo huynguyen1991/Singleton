94baab82492871607f5f20d1f8c780853f32cc01
huynguyen <huy.nguyen1@ntq-solution.com.vn>
2021-09-16 14:21:46 +0700

    init

---
+++ Singleton/Singleton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Singleton
+//
+//  Created by Huy Nguyễn on 16/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
